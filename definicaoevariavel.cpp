c0e41e8b7f0959fa12e138d22b371f1f46bc3ee3
jagunço da silva <89981741+tiqui@users.noreply.github.com>
2023-10-11 21:12:59 -0300

    Definição E Variável
    11/10/2023

---
+++ definicaoevariavel.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+
+int main()
+{
+ bool var1 = true;
+ cgat var2 = " c ";
+ int  var3 = 94, var4 = 118;
+ double = var5 = 19.953, var6 = 13.3432e2;
+ std::cout << "var1 = " << var1 << " , tamanho = " << sizef(var1) << " Byte " << std::endl;
+ std::cout << "var2 = " << var2 << " , tamanho = " << sizef(var2) << " Byte " << std::endl;
+ std::cout << "var3 = " << var3 << " , tamanho = " << sizef(var3) << " Byte " << std::endl;
+ std::cout << "var4 = " << var4 << " , tamanho = " << sizef(var4) << " Byte " << std::endl;
+ std::cout << "var5 = " << var5 << " , tamanho = " << sizef(var5) << " Byte " << std::endl;
+ std::cout << "var6 = " << var6 << " , tamanho = " << sizef(var6) << " Byte " << std::endl;
+
+
+}

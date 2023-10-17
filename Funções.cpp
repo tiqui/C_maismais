04a4b44c38f231041e0871c372a881fa543bd94d
jagunço da silva <89981741+tiqui@users.noreply.github.com>
2023-10-17 11:52:05 -0300

    Globais e não. E um recurso novo chamado define

---
+++ Funções.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+//constante e definicao de ´´ global
+#define pi 3.14;
+#define kleber cout << "o proprio kleber nao tem jeito!\n";
+void Assine()
+{
+    cout << "Assine o onlyfans do bob esponja\n";
+
+}
+// & esse bglhinho conecta os universos, se colocar isso no x ou y vai alterar a e b la em baixo
+//fora isso as variaveis nao se alteram
+int somar(int x, int y)
+{
+    int t;
+    t=1;
+
+    int soma;
+    soma = x+y;
+    return soma;
+    
+}
+
+int main()
+{
+    int t;
+    t=0;
+
+
+   Assine();
+
+   int a=5, b=4;
+   int s;
+   s = somar(a,b);
+   cout << "Soma: " << s << endl;
+
+cout << "Valor de t: " << t << endl;
+
+    cout << pi;
+    kleber;
+
+    return 0;
+}

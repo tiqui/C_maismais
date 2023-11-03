a855afba9aed39e119fccdc229e1d2360fc714db
jagunço da silva <89981741+tiqui@users.noreply.github.com>
2023-11-03 20:19:42 -0300

    Add files via upload

---
+++ Teste infinito.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std; 
+int main() {
+    long long numero = 1;
+
+    while (true) {
+        cout << numero << endl;
+        numero++;
+    }
+
+    return 0;
+}
